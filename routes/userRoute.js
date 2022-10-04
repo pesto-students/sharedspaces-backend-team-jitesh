8c7e35563fc56ad32267e3ba947db928d4e50838
pradeepmalvi <p.malviyaaa@gmail.com>
2022-10-04 17:05:50 +0530

    social login with google API created

--- routes/userRoute.js
+++ routes/userRoute.js
@@ -5,6 +5,7 @@ const {
   getAllUsers,
   uploadUserProfile,
   getUser,
+  userSocial,
 } = require("../controllers/userController");
 const { authenticate, admin } = require("../middleware/authMiddleware");
 
@@ -12,6 +13,7 @@ const router = express.Router();
 
 router.route("/signUp").post(userSignUp);
 router.route("/login").post(userLogin);
+router.route("/social-login").post(userSocial);
 router.route("/getAll").post(admin, authenticate, getAllUsers);
 router.route("/getUser").get(authenticate, getUser);
 router.route("/updateUserProfile/:userId").put(authenticate, uploadUserProfile);
